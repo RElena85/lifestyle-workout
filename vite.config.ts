0456b214df5f4284112e1e4fe18182c76bcb85ba
Relena <rafael_elena_alba@outlook.com>
2025-03-01 12:52:17 +0100

    Add serve script to package.json and update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // This is crucial for proper asset loading
+  base: '/lifestyle/workout/', // This is crucial for proper asset loading
   // resolve: {
   //   alias: {
   //     '@': path.resolve(__dirname, './src'),
@@ -15,14 +15,12 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
     sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       return id.startsWith('node_modules') ? 'vendor' : '';
+    //     },
+    //   },
+    // },
   },
 })
